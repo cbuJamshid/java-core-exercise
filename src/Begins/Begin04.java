ce6306435a3c4d6263c1d41cf7d036327ccc3071
cbuJamshid <jabdukholov@gmail.com>
2025-02-19 00:09:58 +0500

    added

--- src/Begins/Begin04.java
+++ src/Begins/Begin04.java
@@ -21,4 +21,3 @@ public class Begin04 {
 
     }
 }
-
--- src/Begins/Begin05.java
+++ src/Begins/Begin05.java
@@ -1,8 +1,16 @@
 package Begins;
 
+import java.util.Scanner;
+
 public class Begin05 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("R1 ni kiriting:");
+        float R1 = scan.nextFloat();
+
+        final float pi=3.14f;
+        System.out.println("L uzunligi:"+2*pi*R1);
+        System.out.println("S yuzasi:"+pi*R1*R1);
     }
 }
 
