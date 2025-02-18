752cdba29b8e38891ad86a8a85f2bd4bfcb4ab26
cbuJamshid <jabdukholov@gmail.com>
2025-02-19 00:04:12 +0500

    added

--- src/Begins/Begin04.java
+++ src/Begins/Begin04.java
@@ -1,8 +1,24 @@
 package Begins;
+import java.util.Scanner;
+
+//Begin13
 
 public class Begin04 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("R1 ni kiriting:");
+        float R1 = scan.nextFloat();
+        System.out.println("R2 ni kiriting:");
+        float R2 = scan.nextFloat();
+        final float pi=3.14f;
+        System.out.println("S1 ning yuzasi:"+pi*R1*R1);
+        System.out.println("S2 ning yuzasi:"+pi*R2*R2);
+        if (R1 > R2){
+            System.out.println("S3 ning yuzasi:"+(pi*R1*R1-pi*R2*R2));
+        } else{
+            System.out.println("R1 kichkina R2");
+        }
+
     }
 }
 
