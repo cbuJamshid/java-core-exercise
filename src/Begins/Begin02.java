dd0abdd0afa03e71de4966935c8b0c624f1a15ed
cbuJamshid <jabdukholov@gmail.com>
2025-02-18 23:37:56 +0500

    added

--- src/Begins/Begin02.java
+++ src/Begins/Begin02.java
@@ -1,7 +1,14 @@
 package Begins;
+import java.util.Scanner;
 
+//Begin7
 public class Begin02 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("a ni kiriting:");
+        final float PI = 3.14f;
+        float a = scan.nextFloat();
+        System.out.println("S:"+ PI * a * a);
+        System.out.println("L:" + a * 2*PI);
     }
 }
