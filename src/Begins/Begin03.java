b7a889ed5f9d46312bb2848040099b41abd18574
cbuJamshid <jabdukholov@gmail.com>
2025-02-18 23:43:10 +0500

    added

--- src/Begins/Begin03.java
+++ src/Begins/Begin03.java
@@ -1,8 +1,15 @@
 package Begins;
+import java.util.Scanner;
 
+// Begin9
 public class Begin03 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("a ni kiriting:");
+        float a = scan.nextFloat();
+        System.out.println("b ni kiriting:");
+        float b = scan.nextFloat();
+        System.out.println("S:"+ Math.sqrt(a*b));
     }
 }
 
